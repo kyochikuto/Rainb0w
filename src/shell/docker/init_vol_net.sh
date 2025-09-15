b4a009cd053e1620b64987fb90da0253aa09ad92
Kyōchikutō | キョウチクトウ <kyochikuto@proton.me>
2025-09-15 16:14:04 +0330

    fix: enable ipv6 connectivity in Docker

--- src/shell/docker/init_vol_net.sh
+++ src/shell/docker/init_vol_net.sh
@@ -3,10 +3,10 @@ source $PWD/src/shell/base/colors.sh
 
 if ! docker network list | awk '{print $2}' | grep -q '^web$'; then
     echo -e "${B_GREEN}>> Creating a Docker network for NGINX's front-end ${RESET}"
-    docker network create web >/dev/null
+    docker network create --ipv6 web >/dev/null
 fi
 
 if ! docker network list | awk '{print $2}' | grep -q '^proxy-tier$'; then
     echo -e "${B_GREEN}>> Creating a Docker network for proxies ${RESET}"
-    docker network create proxy-tier >/dev/null
+    docker network create --ipv6 proxy-tier >/dev/null
 fi
