1c61873a788e1da08957bc9b4e75a46c57928956
Kyōchikutō | キョウチクトウ <kyochikuto@proton.me>
2025-09-20 00:08:18 +0330

    chore: tweak kernel buffer settings

--- src/shell/performance/tune_kernel_net.sh
+++ src/shell/performance/tune_kernel_net.sh
@@ -16,7 +16,9 @@ echo "net.ipv4.tcp_notsent_lowat=16384" | tee -a /etc/sysctl.d/99-x-network-tune
 echo "net.ipv4.tcp_slow_start_after_idle=0" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
 # UDP optimizations
 echo -e "${B_GREEN}>> Tuning UDP buffer size ${RESET}"
-echo "net.core.rmem_max=4000000" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
-echo "net.core.wmem_max=4000000" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
+echo "net.core.rmem_max=16777216" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
+echo "net.core.wmem_max=16777216" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
+echo "net.ipv4.udp_rmem_min=16384" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
+echo "net.ipv4.udp_wmem_min=16384" | tee -a /etc/sysctl.d/99-x-network-tune.conf >/dev/null
 sysctl -p /etc/sysctl.d/99-x-network-tune.conf
 sleep 1
